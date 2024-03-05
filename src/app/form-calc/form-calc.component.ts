7b0303e96f265f734c9038af2318c20f97b0e11e
coelhopedro20032004@gmail.com <coelhopedro20032004@gmail.com>
2024-03-05 11:07:48 -0300

    subindo o restante das operações

--- src/app/form-calc/form-calc.component.ts
+++ src/app/form-calc/form-calc.component.ts
@@ -9,9 +9,25 @@ export class FormCalcComponent {
 num1: number = 0;
 num2: number = 0;
 sum: number = 0;
-
+mult: number = 0;
+sub: number = 0;
+div: number = 0;
 onclicksum(){
   this.sum = this.num1 + this.num2;
+  this.mult = this.num1 * this.num2;
+  this.div = this.num1 / this.num2;
+  this.sub = this.num1 - this.num2;
+}
+onclicksub(){
+  this.sub = this.num1 - this.num2;
+}
+
+onclickdiv(){
+  this.div = this.num1 / this.num2;
+}
+
+onclickmult(){
+  this.mult = this.num1 * this.num2;
 }
 
 
