ef3016c1ceb0996ffe57dcf4d834a4f44334c949
coelhopedro20032004@gmail.com <coelhopedro20032004@gmail.com>
2024-03-05 11:20:06 -0300

    corrigindo e otimizando o código da classe

--- src/app/form-calc/form-calc.component.ts
+++ src/app/form-calc/form-calc.component.ts
@@ -8,26 +8,22 @@ import { Component } from '@angular/core';
 export class FormCalcComponent {
 num1: number = 0;
 num2: number = 0;
-sum: number = 0;
-mult: number = 0;
-sub: number = 0;
-div: number = 0;
+resp: number = 0;
+
 onclicksum(){
-  this.sum = this.num1 + this.num2;
-  this.mult = this.num1 * this.num2;
-  this.div = this.num1 / this.num2;
-  this.sub = this.num1 - this.num2;
+  this.resp = this.num1 + this.num2;
+  
 }
 onclicksub(){
-  this.sub = this.num1 - this.num2;
+  this.resp = this.num1 - this.num2;
 }
 
 onclickdiv(){
-  this.div = this.num1 / this.num2;
+  this.resp = this.num1 / this.num2;
 }
 
 onclickmult(){
-  this.mult = this.num1 * this.num2;
+  this.resp = this.num1 * this.num2;
 }
 
 
